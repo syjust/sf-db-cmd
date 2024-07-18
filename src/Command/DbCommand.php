604f986f961ff46674fff8140239df4b820d7427
Sylvain Just <sylvain@justdevops.com>
2024-07-18 13:44:57 +0200

    feat: remove unnecessary dep

--- src/Command/DbCommand.php
+++ src/Command/DbCommand.php
@@ -2,7 +2,6 @@
 namespace Syjust\SfDbCmd\Command;
 
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bridge\Doctrine\ManagerRegistry;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -22,7 +21,6 @@ class DbCommand extends Command
 {
     private SymfonyStyle $io;
     private EntityManagerInterface $em;
-    private ManagerRegistry $manager;
 
     #[Required]
     public function setEm(EntityManagerInterface $em): void
