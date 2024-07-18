abeb312c9adea73ab6a0c6197aff3bc4d1ae91f5
Sylvain Just <sylvain@justdevops.com>
2024-07-18 12:32:21 +0200

    feat: add http-kernel dep & DbCommandBundle

---
+++ src/DbCommandBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Syjust\SfDbCmd;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class DbCommandBundle
+ *
+ * @package Syjust\SfDbCmd
+ */
+class DbCommandBundle extends Bundle
+{
+}
