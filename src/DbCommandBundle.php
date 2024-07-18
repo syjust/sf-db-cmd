41d1704324ae5e5951759d6af82bcc1d79efc577
Sylvain Just <sylvain@justdevops.com>
2024-07-18 15:33:56 +0200

    feat: inject services with dependency injection

--- src/DbCommandBundle.php
+++ src/DbCommandBundle.php
@@ -2,6 +2,8 @@
 
 namespace Syjust\SfDbCmd;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 /**
@@ -14,5 +16,10 @@ class DbCommandBundle extends AbstractBundle
     public function getPath(): string
     {
         return dirname(__DIR__);
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
     }
 }
